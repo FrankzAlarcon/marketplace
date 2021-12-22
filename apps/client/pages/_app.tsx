d2ce819a9782b427b023d8b4d3cc77749e08dec8
Juan Jaramillo <juan.jaramillo02@epn.edu.ec>
2021-12-21 21:54:08 -0500

    bootstrap implementation

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,23 +1,11 @@
 import { AppProps } from 'next/app';
-import Head from 'next/head';
 import './styles.css';
+import '../styles/global.css'
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>Welcome to client!</title>
-      </Head>
-      <div className="app">
-        <header className="flex">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src="/nx-logo-white.svg" alt="Nx logo" width="75" height="50" />
-          <h1>Welcome to client!</h1>
-        </header>
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </div>
+      <Component {...pageProps} />
     </>
   );
 }
