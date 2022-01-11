894cd3f5c29d64637a962531b925a75e42a80d09
Alex Padilla <alex.padilla@epn.edu.ec.com>
2022-01-11 13:00:29 -0500

    Create Colors component

---
+++ apps/client/components/Colors.tsx
@@ -0,0 +1,9 @@
+export const Colors = {
+    primary: "#7749F8",
+    primaryDark: "#5227CC",
+    primaryLight: "#EBE5FC",
+    secondary: "",
+    secondaryDark: "",
+    secondaryLight: "",
+    white: "#FFFF"
+}
