52ab0b78556617ba78d8ede20e801f2ea5620fd4
Alex Padilla <alex.padilla@epn.edu.ec.com>
2022-01-18 15:54:00 -0500

    Remove styled components

--- apps/client/components/Colors.tsx
+++
@@ -1,9 +0,0 @@
-export const Colors = {
-    primary: "#7749F8",
-    primaryDark: "#5227CC",
-    primaryLight: "#EBE5FC",
-    secondary: "",
-    secondaryDark: "",
-    secondaryLight: "",
-    white: "#FFFF"
-}
